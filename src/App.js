211783d64672f090b4557c3975eb39d2c7636276
Seamus <bl4ster9000@gmail.com>
2026-04-18 14:36:16 -0400

    Added more content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,79 @@
-import styles from styles.css;
+import styles from "./styles.css";
+import { projects } from "./Projects";
 const headerNames = {
   aboutMe: "About Me",
   Skills: "Skills",
   Projects: "Projects"
+};
+const aboutMeInformation = {
+  name: "Seamus Doherty",
+  DOB : "March 31st, 2006",
+  University: "Pace University"
 };
 const contactDetails = {
   email: "sd30149p@pace.edu",
   linkedIn: "www.linkedin.com/in/seamus-doherty-1a84173b0",
   gitHub: "https://github.com/SeamusDoherty1"
 };
-
-export default function Headers() {
-  return (
+function aboutMeFunc(){
+  return(
     <>
-    <h1>{headerNames.aboutMe}, {headerNames.Skills}, {headerNames.Projects}</h1>
+      <h1>{headerNames.aboutMe}</h1>
+      <p1>{aboutMeInformation.name},<br></br>{aboutMeInformation.DOB}, <br></br>{aboutMeInformation.University}</p1>
+      
     </>
   );
+}
+function skillsFunc(){
+  return(
+    <>
+      <h1>{headerNames.Skills}</h1>
+      <ul>
+        <li>Javascript</li>
+        <li>HTML</li>
+        <li>Java</li>
+        <li>Python</li>
+        <li>Experience with Data Prediction software</li>
+        <li>Experience with API Navigation and Application</li>
+      </ul>
+    </>
+  )
+}
+function projectsFunc(){
+  return(
+    <>
+      <h1>{headerNames.Projects}</h1>
+      <ul>
+        {projects.map(project => 
+          <li>
+            <img
+              src= {project.img}
+              alt = {project.name}
+            />
+            <p>
+              <b>{project.name}:</b>
+              {' ' + project.description + ' '}
+            </p>
+          </li>
+        )}
+      </ul>
+    </>
+  )
+}
+export default function Headers() {
+   return (
+     <>
+     <div>
+      {aboutMeFunc()}
+     </div>
+     <div>
+      {skillsFunc()}
+     </div>
+     <div>
+      {projectsFunc()}
+     </div>
+     </>
+   );
 
 }
 
---
+++ src/Projects.js
@@ -0,0 +1,11 @@
+export const projects = [{
+    id: 0,
+    name: "SOBotz Scouting Application",
+    description: "This application was used to scout out information on the top performing teams in each catagory. This project taught me not only HTML, CSS, and Javascript, but how to navigate APIs and API key integration.",
+    img: ".\public\SoBotzApplication.png",
+} , {
+    id: 1,
+    name: "Python Data Management",
+    description: "This application sorts through Grades and amount of hours studied. then it predicts if a student will pass or fail through their study hours.",
+    img: ".\public\ThonnyFile.png"
+}];
